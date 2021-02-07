9b546b9e71ea5eb7847884b3a4dbf26288af8bc3
minderjin <jiniusk@gmail.com>
2021-02-07 12:30:50 +0000

    input/output

--- outputs.tf
+++ outputs.tf
@@ -17,3 +17,30 @@ output "private_subnets" {
 output "private_subnets_cidr_blocks" {
   value = module.vpc.private_subnets_cidr_blocks
 }
+
+
+# azs
+# database_route_table_association_ids
+# database_route_table_ids
+# database_subnet_group
+# database_subnets
+# database_subnets_cidr_blocks
+# igw_id
+# name
+# nat_ids
+# nat_public_ips
+# natgw_ids
+# private_nat_gateway_route_ids
+# private_route_table_association_ids
+# private_route_table_ids
+# private_subnets
+# private_subnets_cidr_blocks
+# public_internet_gateway_route_id
+# public_route_table_association_ids
+# public_route_table_ids
+# public_subnets
+# public_subnets_cidr_blocks
+# vpc_cidr_block
+# vpc_endpoint_s3_id
+# vpc_id
+# vpc_secondary_cidr_blocks
--- variables.tf
+++ variables.tf
@@ -16,8 +16,47 @@ variable "create_database_subnet_route_table" {}
 variable "create_database_internet_gateway_route" {}
 
 
-# variable "amis" {}
+# variable "create_vpc" {}
+# variable "create_igw" {}
 
+# variable "create_database_internet_gateway_route" {}
+# variable "create_database_nat_gateway_route" {}
+# variable "create_database_subnet_group" {}
+# variable "create_database_subnet_route_table" {}
+# variable "database_subnets" {}
+
+# variable "s3_endpoint_type" {}
+# variable "secondary_cidr_blocks" {}
+
+# variable "enable_dns_support" {}
+# variable "enable_nat_gateway" {}
+# variable "enable_s3_endpoint" {}
+
+# variable "private_subnet_suffix" {}
+# variable "public_subnet_suffix" {}
+# variable "database_subnet_suffix" {}
+
+# variable "tag" {}
+# variable "vpc_tags" {}
+# variable "igw_tags" {}
+# variable "nat_eip_tags" {}
+# variable "nat_gateway_tags" {}
+# variable "private_subnet_tags" {}
+# variable "private_route_table_tags" {}
+# variable "public_route_table_tags" {}
+# variable "public_subnet_tags" {}
+# variable "vpc_endpoint_tags" {}
+# variable "database_subnet_tags" {}
+# variable "database_route_table_tags" {}
+# variable "database_subnet_group_tags" {}
+
+# variable "enable_apigw_endpoint" {}
+# variable "apigw_endpoint_private_dns_enabled" {}
+# variable "apigw_endpoint_security_group_ids" {}
+# variable "apigw_endpoint_subnet_ids" {}
+
+
+# variable "amis" {}
 # variable "amis" {
 #   type = map(string)
 #   default = {
