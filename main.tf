94e64dfa00fbdd09e30e6c348825529a7fe438d0
minderjin <jiniusk@gmail.com>
2021-02-07 01:43:02 +0000

    add terraform.tfvars

--- main.tf
+++ main.tf
@@ -15,17 +15,28 @@ provider "aws" {
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
+  name = var.name
+  cidr = var.cidr
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
 
-  enable_nat_gateway = false
+  enable_nat_gateway = var.enable_nat_gateway
 
   tags = {
     Terraform   = "true"
     Environment = "dev"
   }
+
+
+  # name = "my-vpc"
+  # cidr = "10.0.0.0/16"
+
+  # azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  # private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  # public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  # enable_nat_gateway = false
+
 }
--- variables.tf
+++ variables.tf
@@ -1,12 +1,27 @@
+variable "region" {}
+variable "amis" {}
 
-variable "region" {
-  default = "us-west-2"
-}
+variable "name" {}
+variable "region" {}
+variable "azs" {}
 
-variable "amis" {
-  type = map(string)
-  default = {
-    "us-east-1" = "ami-b374d5a5"
-    "us-west-2" = "ami-fc0b939c"
-  }
-}
+variable "cidr" {}
+variable "private_subnets" {}
+variable "public_subnets" {}
+
+variable "enable_nat_gateway" {}
+
+variable "amis" {}
+
+
+# variable "region" {
+#   default = "us-west-2"
+# }
+
+# variable "amis" {
+#   type = map(string)
+#   default = {
+#     "us-east-1" = "ami-b374d5a5"
+#     "us-west-2" = "ami-fc0b939c"
+#   }
+# }
